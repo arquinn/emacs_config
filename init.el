a4f0b51b682eed8c20d3d25be83d057b1c0d88e6
Andrew Quinn <aquinn1@ucsc.edu>
2024-08-22 15:02:47 -0700

    updates on server side

--- init.el
+++ init.el
@@ -1,5 +1,4 @@
 ;;; package -- summary
-
 ;; Configuration that I've found useful for using emacs on c/c++
 ;; development.
 
@@ -27,16 +26,16 @@
 (defvar user-home-directory (concat (getenv "HOME") "/"))
 (setq user-emacs-directory (concat user-home-directory ".emacs.d/"))
 
+;; Silence the haters (warnings)
+(setq warning-minimum-level :error)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Theme and Display settings
 
-;; use line modes, show line numbers, and show column number in buffer
-(global-linum-mode t)
+;;; add line numbers in programming modes.
+(add-hook 'prog-mode-hook #'display-line-numbers-mode)
 (setq column-number-mode t)
 
-(defvar linum-format)
-(setq linum-format "%4d \u2502")
 
 ;; fill-column at 80 by default
 (setq-default fill-column 80)
@@ -99,12 +98,12 @@
  '(custom-safe-themes
    '("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" default))
  '(package-selected-packages
-   '(markdown-mode rust-mode ws-butler clang-format color-theme-sanityinc-tomorrow color-theme protobuf-mode function-args sr-speedbar helm stickyfunc-enhance flycheck)))
+   '(markdown-mode rust-mode ws-butler clang-format
+     color-theme-sanityinc-tomorrow color-theme protobuf-mode
+      function-args sr-speedbar helm stickyfunc-enhance flycheck)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(speedbar-directory-face ((t (:inherit font-lock-string-face)))))
-
-;;;   '(rust-mode ws-butler clang-format color-theme-sanityinc-tomorrow color-theme protobuf-mode function-args sr-speedbar helm stickyfunc-enhance flycheck fill-column-indicator)))
